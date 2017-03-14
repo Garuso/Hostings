cfbb533b1469c5f92ebf110f070b5fd41b5bbee3
root <root@h22.megalink.com>
2017-03-14 00:32:17 -0400

    Cambio en script Raid Logico

--- verifica_raid.sh
+++ verifica_raid.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+# SCRIPT Verifica Raid Logico
+
 PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/bin:/sbin
 
 export PATH
 
-
 ESTADO=$(/sbin/dmraid -s | grep '^status' | awk -F: '{print $2 }' | sed 's/ //')
-
 
 if [ $ESTADO != "ok" ];then
 SALIDA=$(/sbin/dmraid -r)
 SALIDA1="#######################\n"$(/sbin/dmraid -s )"\n#########################\n"$SALIDA
 echo -ne "$SALIDA1" | mail -s "Error en RAID H7" eruiz@megalink.com  -c jcgalvez@megalink.com
 fi
+
